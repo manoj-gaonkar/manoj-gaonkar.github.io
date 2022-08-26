7458971b35623c10ae9b7ae9c22fb26a9dfb9817
manoj-gaonkar <manojgaonkar7648@gmail.com>
2022-08-26 10:54:25 +0530

    nav panel test

---
+++ script.js
@@ -0,0 +1,20 @@
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+                back: '#151515',
+                secondary: '#EAEAEA',
+                sidetriangle: '#4D4D4D',
+          }
+            },
+             letterSpacing: {
+      tighter: '-0.05em',
+      tight: '-0.025em',
+      normal: '0em',
+      wide: '0.025em',
+      wider: '0.05em',
+        widest: '0.1em',
+      morewider: '0.3em',
+    },
+      }
+    }
